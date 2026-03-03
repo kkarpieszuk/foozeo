d2b1a8d7ef6097edfa1fbd8d30c941f90f2f9f5c
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2026-03-03 07:37:33 +0100

    Task 2. Add empty script in footer

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@ function foozeo_enqueue_styles() {
     wp_enqueue_style('foozeo-style', get_stylesheet_uri(), [], '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'foozeo_enqueue_styles');
+
+function foozeo_enqueue_scripts() {
+    wp_enqueue_script('foozeo-script', get_stylesheet_directory_uri() . '/assets/js/script.js', [], '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'foozeo_enqueue_scripts');
